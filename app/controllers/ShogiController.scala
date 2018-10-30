2b3bf3f877e5b981dcdd48182afb61310a2f5def
ni431str <ni431str@htwg-konstanz.de>
2018-10-30 22:13:22 +0100

    Play almost everything in text form works.

--- app/controllers/ShogiController.scala
+++ app/controllers/ShogiController.scala
@@ -1,20 +1,68 @@
 package controllers
 
 import javax.inject._
-
 import play.api.mvc._
 import de.htwg.se.Shogi.Shogi
-
+import de.htwg.se.Shogi.aview.Tui
+import de.htwg.se.Shogi.controller.controllerComponent.MoveResult
 @Singleton
 class ShogiController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
   val gameController = Shogi.controller
+  val tui = new Tui(gameController)
   def shogiAsText =  gameController.boardToString()
   def shogi = Action {
-    Ok(views.html.index(shogiAsText))
+    Ok(gameController.boardToString())
   }
 
-  def newBoard = Action {
+  def emptyBoard = Action {
+    gameController.createEmptyBoard()
+    Ok(gameController.boardToString())
+  }
+
+  def newBoard() = Action {
     gameController.createNewBoard()
-    Ok(views.html.sudoku(shogiAsText))
+    Ok(gameController.boardToString())
+  }
+
+  def undo = Action {
+    gameController.undoCommand
+    Ok(gameController.boardToString())
+  }
+
+  def redo = Action {
+    gameController.redoCommand
+    Ok(gameController.boardToString())
+  }
+
+  def possibleMoves(x:Int, y:Int) = Action {
+    val list = gameController.getPossibleMoves(x,y)
+    if(list.isEmpty){
+      Ok(gameController.boardToString() + "\n\n" + "There are no moves!")
+    }else{
+      Ok(gameController.boardToString() + "\n\n" + list.toString())
+    }
+  }
+
+  def movePiece(x:Int, y:Int, i:Int, j:Int) = Action {
+    gameController.movePiece((x, y), (i, j)) match {
+      case MoveResult.invalidMove => Ok(gameController.boardToString() + "\n\n" + "<h1>This move is not valid</h1>")
+      case MoveResult.validMove => {
+        if (gameController.promotable((i, j))) {
+        Ok(gameController.boardToString() + "\n\n" +  "Do u want to promote your piece? /y for yes!")
+      }else
+        Ok(gameController.boardToString())
+    }
+      case MoveResult.kingSlain => Ok("King Slain YAY")
+      }
+  }
+
+  def promotePiece(x:Int, y:Int, i:Int, j:Int, promotion: String) = Action {
+    if(promotion == "y")
+      gameController.promotePiece(i,j)
+    Ok(gameController.boardToString())
+  }
+
+  def about = Action {
+    Ok(views.html.aboutTheGame())
   }
 }
