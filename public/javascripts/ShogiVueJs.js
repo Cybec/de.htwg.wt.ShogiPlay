5dc00624dd0824df0372adf3e6b6b68866e96384
me431zey <32568508+me431zey@users.noreply.github.com>
2019-01-13 20:22:58 +0100

    Vue implementiert

---
+++ public/javascripts/ShogiVueJs.js
@@ -0,0 +1,310 @@
+var lastPM = "";
+var last_clicked_id = "";
+var kingSlainEnd = false;
+
+//Helper Simulation
+var helper = 0;
+var helper2 = 0;
+var simuList;
+var simuList_All = [];
+var simuCount = 1;
+
+//Load Simu on start
+$(document).ready(function () {
+    console.log("Document is ready");
+    //get simulation list
+    $.ajax(
+        {
+            type: 'GET',
+            url: "SimuToJson",
+            dataType: 'Json',
+
+            success: function (simu) {
+                for (var k in simu) {
+                    simuList_All.push(simu[k])
+                }
+                simuList = simuList_All[0];
+            }
+        }
+    );
+});
+
+//Close About
+new Vue({
+    el: '#closeModal',
+    methods: {
+        close: function () {
+            document.getElementById('AboutModal').style.display = "none";
+        }
+    }
+});
+$(document).click(function (event) {
+    if (event.target.id === "AboutModal") {
+        document.getElementById('AboutModal').style.display = "none";
+    }
+});
+
+//laod Playfield on startup and init "const PlayField" + click piece event handler
+const PlayField = new Vue({
+    el: '#PlayingField',
+    data: {
+        size: 9,
+        posMoves: [],
+        playerFirstCon: [],
+        playerSecondCon: [],
+        img: []
+    },
+    created() {
+        fetch('http://localhost:9000/boardToJson')
+            .then(response => response.json())
+            .then(json => {
+                update(this);
+            })
+    },
+    methods: {
+        pieceClicked: function (event) {
+            if (!kingSlainEnd) {
+                clickOnBoard(event.currentTarget.id);
+            }
+        }
+    }
+});
+
+//Navbar
+new Vue({
+    el: '#Navbar',
+    methods: {
+        newGame: function () {
+            newGame();
+        },
+        emptyGame: function () {
+            fetch('empty')
+                .then(response => response.json())
+                .then(json => {
+                    update();
+                })
+        },
+        saveGame: function () {
+            fetch('save')
+                .then(response => response.json())
+                .then(json => {
+                    update();
+                })
+        },
+        loadGame: function () {
+            fetch('load')
+                .then(response => response.json())
+                .then(json => {
+                    update();
+                })
+        },
+        aboutGame: function () {
+            document.getElementById('AboutModal').style.display = "block";
+        },
+        undoGame: function () {
+            fetch('undo')
+                .then(response => response.json())
+                .then(json => {
+                    update();
+                })
+        },
+        redoGame: function () {
+            fetch('redo')
+                .then(response => response.json())
+                .then(json => {
+                    update();
+                })
+        },
+        simulation: function () {
+            Simulation()
+        },
+        logo: function () {
+            update();
+        }
+    }
+});
+
+function update() {
+    fetch('boardToJson')
+        .then(response => response.json())
+        .then(json => {
+            //Reset Conquered
+            PlayField.playerFirstCon = [];
+            PlayField.playerSecondCon = [];
+
+            let i;
+            for (i = 0; i < json.board.length; i++) {
+                Vue.set(PlayField.posMoves, i, json.board[i].posMovs);
+                Vue.set(PlayField.img, i, json.board[i].piece.img);
+            }
+            for (i = 0; i < json.playerFirstConquered.length; i++) {
+                Vue.set(PlayField.playerFirstCon, i, {
+                    img: json.playerFirstConquered[i].img,
+                    name: json.playerFirstConquered[i].pieceName,
+                    posMoves: json.playerFirstConquered[i].posMovs
+                });
+            }
+            for (i = 0; i < json.playerSecondConquered.length; i++) {
+                console.log(json.playerSecondConquered[i].img);
+                Vue.set(PlayField.playerSecondCon, i, {
+                    img: json.playerSecondConquered[i].img,
+                    name: json.playerSecondConquered[i].pieceName,
+                    posMoves: json.playerSecondConquered[i].posMovs
+                });
+            }
+        })
+}
+
+function clickOnBoard(clicked_id) {
+    if (lastPM.includes(clicked_id.replace("-", ","))) {
+        if ('0123456789'.indexOf(last_clicked_id.charAt(0)) !== -1) {
+            var destLink = "mv/" +
+                last_clicked_id.charAt(0) + "/" +
+                last_clicked_id.charAt(2) + "/" +
+                clicked_id.charAt(0) + "/" +
+                clicked_id.charAt(2);
+            $.ajax(
+                {
+                    type: 'GET',
+                    url: destLink,
+                    dataType: 'html',
+
+                    success: function (result) {
+                        if (result === "<p>Promotable</p>") {
+                            Promotable(destLink);
+                        } else if (result === "<p>InvalidMove</p>") {
+                            alert("This move is not possible!")
+                        } else if (result === "<p>KingSlain</p>") {
+                            KingSlain();
+                        }
+                        update();
+                        resetGlobalVal();
+                    }
+                }
+            )
+        } else {
+            $.ajax(
+                {
+                    type: 'GET',
+                    url: "mvcp/"
+                        + last_clicked_id + "/" +
+                        clicked_id.charAt(0) + "/" +
+                        clicked_id.charAt(2),
+                    dataType: 'html',
+
+                    success: function () {
+                        update();
+                        resetGlobalVal();
+                    }
+                }
+            )
+        }
+    }
+
+
+    ResetBlueFields();
+
+    GetPosMovColorize(clicked_id);
+
+    last_clicked_id = clicked_id;
+}
+
+function ResetBlueFields() {
+//Set all Blue Divs back to default
+    lastPM.split("-").forEach(function (item) {
+        var res = item.replace("(", "").replace(")", "").replace(",", "-");
+        if (res.length !== 0) {
+            document.getElementById(res).style.backgroundColor = "#ffebb6";
+        }
+    });
+}
+
+function GetPosMovColorize(clicked_id) {
+//Get possible moves
+    var a = document.getElementById("Label_" + clicked_id).innerText.split("-");
+    lastPM = document.getElementById("Label_" + clicked_id).innerText;
+    a.forEach(function (item) {
+        var res = item.replace("(", "").replace(")", "").replace(",", "-");
+        if (res.length !== 0) {
+            document.getElementById(res).style.backgroundColor = "blue";
+        }
+    });
+}
+
+function Promotable(currentLink) {
+    var linkYesNo = currentLink;
+    if (confirm("Do you want to Promote the Piece?")) {
+        linkYesNo += "/y";
+    } else {
+        linkYesNo += "/n";
+    }
+    $.ajax(
+        {
+            type: 'GET',
+            url: linkYesNo,
+            dataType: 'html',
+
+            success: function () {
+                update();
+            }
+        }
+    )
+}
+
+function KingSlain() {
+    kingSlainEnd = true;
+    if (confirm("King was slain, you won!!!\n" +
+        "Confirm to begin a new game!")) {
+        newGame();
+    }
+}
+
+function newGame() {
+    kingSlainEnd = false;
+    lastPM = "";
+    last_clicked_id = "";
+
+    simuList = simuList_All[0];
+    simuCount = 0;
+    helper = 0;
+    helper2 = 0;
+    fetch('new')
+        .then(response => response.json())
+        .then(json => {
+            update();
+        })
+}
+
+function resetGlobalVal() {
+    lastPM = "";
+    last_clicked_id = "";
+}
+
+function Simulation() {
+    $('#' + simuList[helper][helper2]).fadeIn(100).fadeIn(100)
+        .fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100)
+        .fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    clickOnBoard(simuList[helper][helper2]);
+
+
+    helper2 += 1;
+    if (helper2 >= 2) {
+        helper2 = 0;
+        helper += 1;
+    }
+    if (helper >= simuList.length) {
+        helper = 0;
+        helper2 = 0;
+        if (simuCount >= simuList_All.length) {
+            alert("Simulation ended!");
+            simuList = simuList_All[0];
+            simuCount = 0;
+            helper = 0;
+            helper2 = 0;
+        } else {
+            simuList = simuList_All[simuCount];
+            simuCount += 1;
+        }
+    }
+}
